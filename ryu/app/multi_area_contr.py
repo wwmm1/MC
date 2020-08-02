769b36206b2168b742b976a8dffd68ebeb912d36
wwmm1 <397920597@qq.com>
2020-08-02 23:32:23 +0800

    type

---
+++ ryu/app/multi_area_contr.py
@@ -0,0 +1,197 @@
+#-*- conding:utf-8 -*-
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER,MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.lib.packet import packet,ethernet,ether_types
+from ryu.ofproto import ofproto_v1_3
+from ryu.topology import event
+from ryu.topology.api import get_switch,get_link,get_host
+import zookeeper_server
+import networkx as nx
+
+
+class Mutlti_Area_Contr(app_manager.RyuApp):
+
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self,*args,**kwargs):
+        super(Mutlti_Area_Contr,self).__init__(*args,**kwargs)
+        self.mac_to_port = {}
+        self.topology_api_app = self
+        self.network = nx.DiGraph()
+
+
+    #switch features
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures,CONFIG_DISPATCHER)
+    def switch_features_hanlder(self,ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+
+        self.add_flow(datapath,0,match,actions)
+
+    #add flow-table
+    def add_flow(self,datapath, priority, match, actions, buffer_id = None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority,match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match,instructions=inst)
+
+        datapath.send_msg(mod)
+
+    #packet_in packet
+    @set_ev_cls(ofp_event.EventOFPPacketIn,MAIN_DISPATCHER)
+    def packet_in_handler(self,ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        dpid = datapath.id
+        #dpid:{mac:port}
+        self.mac_to_port.setdefault(dpid,{})
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        #lldp packet
+        lldp_packet = ether_types.ETH_TYPE_LLDP
+
+        dst = eth.dst  #destination mac
+        src = eth.src  #srouce mac
+
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+
+            if msg.buffer_id != ofproto.OFPCML_NO_BUFFER:
+                self.add_flow(datapath,1,match,actions,msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath,1,match,actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFPCML_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath,buffer_id=msg.buffer_id,
+                                  in_port=in_port,actions=actions,data=data)
+        datapath.send_msg(out)
+
+    #get all switch info and all links
+    @set_ev_cls([event.EventSwitchEnter,event.EventSwitchLeave,
+                 event.EventPortAdd,event.EventPortDelete,
+                 event.EventPortModify,event.EventLinkAdd,event.EventLinkDelete],
+                [CONFIG_DISPATCHER,MAIN_DISPATCHER])
+    def get_topology(self,ev):
+        print(dir(ev.link.dst.port_no))
+        #get switch(dpid)
+        switch_list = get_switch(self.topology_api_app,None)
+        switches = [switch.dp.id for switch in switch_list]     #switches
+        # switch = ev.switch.dp.id           #switch dpid
+        # print(ev.switch.dp.id)
+        self.network.add_nodes_from(switches)
+
+        #get src links
+        link_list = get_link(self.topology_api_app,None)
+        # srclinks = [(link.src.dpid,link.dst.dpid,{'attr_dict':{'port':link.src.port_no,'srcmac':link.src.hw_addr}})
+        #             for link in link_list]
+        srclinks = [(link.src.dpid,link.dst.dpid,{'attr_dict':{'port':link.src.port_no}})
+                    for link in link_list]
+        # aa = [link.dst.hw_addr for link in link_list]
+        # print('srclinks:',srclinks)
+        self.network.add_edges_from(srclinks)
+
+        #reverse links
+        dstlinks = [(link.dst.dpid,link.src.dpid,{'attr_dict':{'port':link.dst.port_no}})
+                    for link in link_list]
+        # print('dst_links:',dstlinks)
+        self.network.add_edges_from(dstlinks)
+
+        #get host
+        hosts_list = get_host(self.topology_api_app)
+        hosts = [(host.port.dpid,host.port.port_no,{'attr_dict':{'ip':host.ipv4,'mac':host.mac}})
+                for host in hosts_list]
+
+        if type(ev) == event.EventSwitchLeave:
+            print('swleave:',ev)
+        if type(ev) == event.EventLinkDelete:
+            print('linkdel:',ev)
+        if type(ev) == event.EventLinkAdd:
+            print('linkadd:',ev.link.dst.port_no)
+
+        # get all switches,links,hosts
+        # add_switch_inf_to_ZkServer(switches,srclinks,hosts)
+
+# add switch information to zk_server
+def add_switch_inf_to_ZkServer(switches,srclinks,hosts=None):
+    # switches = '/' + str(switches)
+    # linkes = str(srclinks)
+    #get zk node,judge node whether is null
+    zk = zookeeper_server.Zookeeper_Server('127.0.0.1','4181')
+    # print('switch:',switches)     #('switch:', [1, 2, 3])
+    # print('srclinks:',srclinks)   #('srclinks:', [(2, 3, {'attr_dict': {'port': 3, 'srcmac': '72:f4:db:b7:8f:21'}}),
+                                  # (2, 1, {'attr_dict': {'port': 2, 'srcmac': '22:56:29:00:71:a0'}}),
+                                  # (3, 2, {'attr_dict': {'port': 2, 'srcmac': '36:fa:6e:fe:7f:20'}}),
+                                  # (1, 2, {'attr_dict': {'port': 2, 'srcmac': 'f6:3d:a0:bf:ed:ef'}})])
+
+    # print('hosts:',hosts)        #('hosts:', [(2, 1, {'attr_dict': {'ip': [], 'mac': '96:e9:18:fc:8c:14'}}),
+                                 # (3, 1, {'attr_dict': {'ip': [], 'mac': '0a:c0:04:59:2f:b1'}}),
+                                 # (1, 1, {'attr_dict': {'ip': [], 'mac': '16:31:3d:97:c5:75'}})])
+
+    #check node and values and ,if node or values or host is nothing, add them
+    if zk.jude_node_exists('/controller'):
+        for switcha in switches:
+            #add links between switch and switch
+            for links in srclinks:
+                if zk.jude_node_exists('/controller' + '/' + str(switcha)):
+                    get_node = zk.get_zk_node('/controller' + '/' + str(switcha))[0]
+                    if switcha == links[0]:
+                        node_value = {}
+                        for node in get_node:
+                            if zk.jude_node_exists('/controller' + '/' + str(switcha) + '/' + node):
+                                get_value = zk.get_zk_node('/controller' + '/' + str(switcha) + '/' + node)[1][0]
+                                node_value[str(node)] = eval(get_value)
+                        if links not in node_value.values():
+                            zk.create_zk_node('/controller' + '/' +str(switcha) + '/' + 'link',links)
+                else:
+                    if switcha == links[0]:
+                        zk.create_zk_node('/controller' + '/' + str(switcha) + '/' + 'link', links)
+            #add links between switch and host
+            for host in hosts:
+                if zk.jude_node_exists('/controller' + '/' + str(switcha)):
+                    get_node = zk.get_zk_node('/controller' + '/' + str(switcha))[0]
+                    if switcha == host[0]:
+                        host_node_value = {}
+                        for node in get_node:
+                            if zk.jude_node_exists('/controller' + '/' + str(switcha) + '/' + node):
+                                get_value = zk.get_zk_node('/controller' + '/' + str(switcha) + '/' + node)[1][0]
+                                host_node_value[str(node)] = eval(get_value)
+                        if host not in host_node_value.values():
+                            zk.create_zk_node('/controller' + '/' + str(switcha) + '/' + 'host', host)
+                else:
+                    if switcha == host[0]:
+                        zk.create_zk_node('/controller' + '/' + str(switcha) + '/' + 'host', host)
+
---
+++ ryu/app/zookeeper_server.py
@@ -0,0 +1,57 @@
+#*-* coding:utf-8 -*-
+from kazoo.client import KazooClient
+# from kazoo.recipe.lock import Lock
+
+class Zookeeper_Server:
+
+    def __init__(self,host,port):
+        self.zk = KazooClient(hosts=host+':'+port)
+        self.zk.start()
+
+    #读取zookeeper节点
+    def get_zk_node(self,nodePath):
+        #获取节点下的所有子节点
+        zkNode = self.zk.get_children(nodePath,watch=self.watches())
+        #获取节点的值
+        # zkNodeValue = self.zk.get(nodePath,watch=self.watches())
+        zkNodeValue = self.zk.get(nodePath)
+
+        # self.zk.stop()
+        return zkNode,zkNodeValue
+
+    #设置zookeeper节点
+    def set_zk_node(self,nodePath,setNodeValue):
+        #设置节点的值
+        self.zk.set(nodePath,setNodeValue)
+
+        # self.zk.stop()
+
+    #删除zookeeper节点
+    def del_zk_node(self,nodePath):
+        #recursive为True则删除此节点和所有子节点
+        #recursive为False则当节点有子节点，则抛出NotEmptyError异常
+        self.zk.delete(nodePath,recursive=True)
+
+        # self.zk.stop()
+
+    #创建zookeeper节点
+    def create_zk_node(self,nodePath,nodeValue):
+        #sequence:若为 True 则在你创建节点名后面增加10位数字
+        #makepath：  若为 False 父节点不存在时抛 NoNodeError。若为 True 父节点不存在则创建父节点。默认 False
+        self.zk.create(nodePath,bytes(nodeValue),sequence=True,makepath=True)
+
+        # self.zk.stop()
+
+    #判断节点是否存在
+    def jude_node_exists(self,nodePath):
+        if self.zk.exists(nodePath):
+            # self.zk.stop()
+            return True
+        else:
+            # self.zk.stop()
+            return False
+
+    #监听事件(只能设置在节点的读取上)
+    def watches(self):
+        pass
+
